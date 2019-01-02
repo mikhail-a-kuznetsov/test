1fb59311c5e5a2f9ef955028da46f9126e816460
Mikhail <mikhail.a.kuznetsov@pwc.com>
2019-01-02 14:32:12 +0300

    Это наш первый коммит

---
+++ script.js
@@ -0,0 +1,30 @@
+'use strict'
+
+let a=5;
+console.log(a)
+
+// let answer=confirm ("are you here?");
+// console.log(answer);
+
+// let answer=+prompt("What's your age?", "18")
+// console.log(answer)
+// let arr = ['plum.png', 'orange.jpg', 'apple.bmp'];
+// console.log(typeof(null));
+// console.log(4+" - object")
+
+let incr=10,
+    decr=10;
+
+// incr++;
+// decr--;
+
+// console.log(incr++);
+// console.log(decr--);
+// console.log(incr);
+// console.log(5%2);
+// console.log("2" === 2)
+
+let isChecked = false,
+    isClose = false;
+
+console.log(isChecked || !isClose);
